f50069d84a6567cd99d611178974ecae2bab213a
Антон Вальченко <aavalchenko@edu.hse.ru>
2018-06-23 03:26:58 +0500

    Задание решено

---
+++ Practice--10/Graph.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice__10
+{
+    class Graph<T>
+    {
+        public List<List<int>> Matrix { get; }
+        public int Count { get; private set; }
+        public List<T> Inform { get; }
+        public Graph(int count, string[] input, List<T> inform)
+        {
+            Count = count;
+            Inform = inform;
+            Matrix = MakeMatrix(Count);
+            foreach (string edge in input)
+            {
+                Matrix[Convert.ToInt32(Char.ToString(edge[0]))][Convert.ToInt32(Char.ToString(edge[1]))] = 1;
+                Matrix[Convert.ToInt32(Char.ToString(edge[1]))][Convert.ToInt32(Char.ToString(edge[0]))] = 1;
+            }
+        }
+
+        private static List<List<int>> MakeMatrix(int count)
+        {
+            List<List<int>> matrix = new List<List<int>>(count);
+            for (int i = 0; i < count; i++)
+            {
+                matrix.Add(new List<int>(count));
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    matrix[i].Add(0);
+                }
+            }
+
+            return matrix;
+        }
+
+        public void DelPoints(T inform)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Inform[i].Equals(inform))
+                {
+                    for (int j = 0; j < Count; j++)
+                    {
+                        Matrix[j].RemoveAt(i);
+                    }
+                    Matrix.RemoveAt(i);
+
+                    Count--;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                for (int j = 0; j < Count; j++)
+                {
+                    Console.Write(Matrix[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Practice--10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using InputOutputLib;
+
+namespace Practice__10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph<int> graph = MakeGraph(Get.Int32("Введите количество вершин графа (нумеруются с нуля): ", 1));
+
+            graph.DelPoints(Get.Int32("Введите значение информационного поля для удаления: "));
+            graph.Print();
+            Console.ReadKey();
+        }
+
+        private static Graph<int> MakeGraph(int count)
+        {
+            string[] edges = Get.String("Введите ребра в виде <начало><конец>, разделяя каждое пробелом: ").Split(' ');
+            List<int> infoms = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                infoms.Add(Get.Int32($"Ведите значение для {i} вершины графа: "));
+            }
+            return new Graph<int>(count, edges, infoms);
+        }
+    }
+}
